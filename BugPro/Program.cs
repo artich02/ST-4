6f57d8987045ced89792a82f87e1a9350791c5ca
artich02 <tar8803@gmail.com>
2025-04-07 14:15:24 +0300

    try 1
    try 1

---
+++ BugPro/Program.cs
@@ -0,0 +1,71 @@
+﻿using Stateless;
+
+namespace BugPro;
+
+public class Bug
+{
+    public enum State { Open, Assigned, InProgress, Defered, Resolved, Closed, Reopened }
+    private enum Trigger { Assign, StartProgress, Defer, Resolve, Close, Reopen, Reassign }
+    private StateMachine<State, Trigger> sm;
+
+    public Bug(State state)
+    {
+        sm = new StateMachine<State, Trigger>(state);
+
+        sm.Configure(State.Open)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Close, State.Closed);
+
+        sm.Configure(State.Assigned)
+            .Permit(Trigger.StartProgress, State.InProgress)
+            .Permit(Trigger.Defer, State.Defered)
+            .Permit(Trigger.Close, State.Closed)
+            .Ignore(Trigger.Assign);
+
+        sm.Configure(State.InProgress)
+            .Permit(Trigger.Resolve, State.Resolved)
+            .Permit(Trigger.Defer, State.Defered)
+            .Permit(Trigger.Reassign, State.Assigned);
+
+        sm.Configure(State.Defered)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Close, State.Closed);
+
+        sm.Configure(State.Resolved)
+            .Permit(Trigger.Close, State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        sm.Configure(State.Closed)
+            .Permit(Trigger.Reopen, State.Reopened);
+
+        sm.Configure(State.Reopened)
+            .Permit(Trigger.Assign, State.Assigned)
+            .Permit(Trigger.Close, State.Closed);
+    }
+
+    public void Assign() { sm.Fire(Trigger.Assign); Console.WriteLine("Assign"); }
+    public void StartProgress() { sm.Fire(Trigger.StartProgress); Console.WriteLine("StartProgress"); }
+    public void Defer() { sm.Fire(Trigger.Defer); Console.WriteLine("Defer"); }
+    public void Resolve() { sm.Fire(Trigger.Resolve); Console.WriteLine("Resolve"); }
+    public void Close() { sm.Fire(Trigger.Close); Console.WriteLine("Close"); }
+    public void Reopen() { sm.Fire(Trigger.Reopen); Console.WriteLine("Reopen"); }
+    public void Reassign() { sm.Fire(Trigger.Reassign); Console.WriteLine("Reassign"); }
+
+    public State GetState() { return sm.State; }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var bug = new Bug(Bug.State.Open);
+        bug.Assign();
+        bug.StartProgress();
+        bug.Resolve();
+        bug.Close();
+        bug.Reopen();
+        bug.Assign();
+        bug.Defer();
+        Console.WriteLine($"Final state: {bug.GetState()}");
+    }
+}
---
+++ BugTests/Test1.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+using Stateless;
+
+namespace BugTests
+{
+    [TestClass]
+    public class BugTests
+    {
+        [TestMethod]
+        public void Open_CanAssign_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Open_CanClose_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Open_CannotDefer_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Open);
+            bug.Defer();
+        }
+
+        [TestMethod]
+        public void Assigned_CanStartProgress_ShouldTransitionToInProgress()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.StartProgress();
+            Assert.AreEqual(Bug.State.InProgress, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Assigned_CanDefer_ShouldTransitionToDefered()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Defer();
+            Assert.AreEqual(Bug.State.Defered, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Assigned_CanClose_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Assigned_CannotResolve_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Assigned);
+            bug.Resolve();
+        }
+
+        [TestMethod]
+        public void InProgress_CanResolve_ShouldTransitionToResolved()
+        {
+            var bug = new Bug(Bug.State.InProgress);
+            bug.Resolve();
+            Assert.AreEqual(Bug.State.Resolved, bug.GetState());
+        }
+
+        [TestMethod]
+        public void InProgress_CanDefer_ShouldTransitionToDefered()
+        {
+            var bug = new Bug(Bug.State.InProgress);
+            bug.Defer();
+            Assert.AreEqual(Bug.State.Defered, bug.GetState());
+        }
+
+        [TestMethod]
+        public void InProgress_CanReassign_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.InProgress);
+            bug.Reassign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InProgress_CannotClose_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.InProgress);
+            bug.Close();
+        }
+
+        [TestMethod]
+        public void Defered_CanAssign_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.Defered);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Defered_CanClose_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Defered);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Defered_CannotResolve_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Defered);
+            bug.Resolve();
+        }
+
+        [TestMethod]
+        public void Resolved_CanClose_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Resolved);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Resolved_CanReopen_ShouldTransitionToReopened()
+        {
+            var bug = new Bug(Bug.State.Resolved);
+            bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Resolved_CannotAssign_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Resolved);
+            bug.Assign();
+        }
+
+        [TestMethod]
+        public void Closed_CanReopen_ShouldTransitionToReopened()
+        {
+            var bug = new Bug(Bug.State.Closed);
+            bug.Reopen();
+            Assert.AreEqual(Bug.State.Reopened, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Closed_CannotDefer_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Closed);
+            bug.Defer();
+        }
+
+        [TestMethod]
+        public void Reopened_CanAssign_ShouldTransitionToAssigned()
+        {
+            var bug = new Bug(Bug.State.Reopened);
+            bug.Assign();
+            Assert.AreEqual(Bug.State.Assigned, bug.GetState());
+        }
+
+        [TestMethod]
+        public void Reopened_CanClose_ShouldTransitionToClosed()
+        {
+            var bug = new Bug(Bug.State.Reopened);
+            bug.Close();
+            Assert.AreEqual(Bug.State.Closed, bug.GetState());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Reopened_CannotResolve_ShouldThrowException()
+        {
+            var bug = new Bug(Bug.State.Reopened);
+            bug.Resolve();
+        }
+    }
+}
